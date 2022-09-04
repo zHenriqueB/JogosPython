c802f6b231e8f4cadf433d77a3364082211e9f29
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-04 18:16:56 -0300

    update

--- jogoAdivinhação/index.py
+++ jogoAdivinhação/index.py
@@ -1,32 +1,32 @@
 
-print("----------------------------------------------------------")
+print("--------------------------------------------------------")
 print("Bem vindo ao jogo de adivinhação")
-print("----------------------------------------------------------")
+print("--------------------------------------------------------")
 
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada =1
 
-chute = input("Digite o seu numero :    ")
-
-print("você digitou o numero", chute)
-acertou = numero_secreto == int(chute)
-maior=  int(chute) > numero_secreto
-menor=  int(chute) < numero_secreto
-
-
-if (acertou):
-    print("Você acertou")
-
-else:
-    if (menor): 
-        print("chutou um numero mais baixo que o numero secreto")
-        print("errou")
-        print("Game Over")
-    elif (maior): 
-        print("chutou um numero mais alto que o numero secreto ")
-        print("errou")
-        print("Game Over")
-    
-    
-    
+while (rodada <= total_de_tentativas):
+    print("tentativa  :", rodada, "de", total_de_tentativas )
+    chute = input("Digite o seu numero :    ")
    
+    acertou = numero_secreto == int(chute)
+    maior=  int(chute) > numero_secreto
+    menor=  int(chute) < numero_secreto
+    
+    if (acertou):
+        print("Você acertou")
+    else:
+        if (menor): 
+            print("chutou um número mais baixo que o numero secreto")
+            print("errou")
+        elif (maior): 
+            print("chutou um número mais alto que o numero secreto ")
+            print("errou")
+        if( rodada == 3):
+         print("GAME OVER")
+         rodada = rodada + 1
+    
+    
