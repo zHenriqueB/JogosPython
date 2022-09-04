819005d667183227ff9ac0bd59eea6a1fcb2e84f
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-04 19:19:10 -0300

    Update

--- jogoAdivinhação/index.py
+++ jogoAdivinhação/index.py
@@ -1,4 +1,8 @@
 
+from ast import Break
+from bdb import Breakpoint
+
+
 print("--------------------------------------------------------")
 print("Bem vindo ao jogo de adivinhação")
 print("--------------------------------------------------------")
@@ -8,16 +12,22 @@ numero_secreto = 42
 total_de_tentativas = 3
 rodada =1
 
-while (rodada <= total_de_tentativas):
+for rodada in range(rodada,total_de_tentativas+1):
     print("Tentativa {} de {}".format(rodada,total_de_tentativas) )
-    chute = input("Digite o seu numero :    ")
+    chute = input("Digite o seu numero entre 1 e 100:    ")
+    
+    if (int(chute)<1 or int(chute) >100 ):
+        print("Você deve digitar um numero de 1 a 100 !")
+        continue 
    
     acertou = numero_secreto == int(chute)
     maior=  int(chute) > numero_secreto
     menor=  int(chute) < numero_secreto
     
     if (acertou):
-        print("Você acertou")
+      print("Você acertou !")
+      print("Fim do jogo !")
+      break
     else:
         if (menor): 
             print("chutou um número mais baixo que o numero secreto")
@@ -27,6 +37,6 @@ while (rodada <= total_de_tentativas):
         
     if( rodada == 3):
          print("GAME OVER")
-    rodada = rodada + 1
+    
     
     
