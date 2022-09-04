983dad8d7ae9e65b30571190ed6021acc7bfc30a
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-04 17:23:59 -0300

    update

--- jogoAdivinhação/index.py
+++ jogoAdivinhação/index.py
@@ -1,2 +1,23 @@
-print("oi")
-var = 8 
+
+
+
+print("----------------------------------------------------------")
+print("Bem vindo ao jogo de adivinhação")
+print("----------------------------------------------------------")
+
+
+numero_secreto = 42
+
+chute = input("Digite o seu numero :    ")
+
+print("você digitou o numero", chute)
+
+
+if (numero_secreto == int(chute)):
+    print("acertou")
+
+else:
+    print("errou")
+    print("Fim do jogo")
+    
+   
