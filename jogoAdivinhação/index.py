5df23ae3f6c257b2e88850fcfee819a3dd28eaec
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-04 17:35:19 -0300

    update

--- jogoAdivinhação/index.py
+++ jogoAdivinhação/index.py
@@ -1,6 +1,4 @@
 
-
-
 print("----------------------------------------------------------")
 print("Bem vindo ao jogo de adivinhação")
 print("----------------------------------------------------------")
@@ -11,13 +9,24 @@ numero_secreto = 42
 chute = input("Digite o seu numero :    ")
 
 print("você digitou o numero", chute)
+acertou = numero_secreto == int(chute)
+maior=  int(chute) > numero_secreto
+menor=  int(chute) < numero_secreto
 
 
-if (numero_secreto == int(chute)):
-    print("acertou")
+if (acertou):
+    print("Você acertou")
 
 else:
-    print("errou")
-    print("Fim do jogo")
+    if (menor): 
+        print("chutou um numero mais baixo que o numero secreto")
+        print("errou")
+        print("Game Over")
+    elif (maior): 
+        print("chutou um numero mais alto que o numero secreto ")
+        print("errou")
+        print("Game Over")
+    
+    
     
    
