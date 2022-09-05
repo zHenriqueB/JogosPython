39049d7b28c645e2e617a19223fee593a32274cc
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-04 22:27:53 -0300

    UPDATE

---
+++ jogoAdivinhação/jogos.py
@@ -0,0 +1,16 @@
+print("--------------------------------------------------------")
+print("------------Escolha o seu jogo-------------")
+print("--------------------------------------------------------")
+
+
+print("Lista de jogos")
+print("(1) JOGO DE ADIVINHAÇÃO")
+print("(2) JOGO DA FORCA")
+
+
+jogo = int(input("Digite o número do jogo"))
+
+if(jogo==1):
+    print("Jogar o jogo de adivinhação")
+elif(jogo==2):
+     print("Jogar o jogo da forca")
