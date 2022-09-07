a7869f194e3575d47fc9301c71b35f5d357fef8b
zHenriqueB <henrique.barros1@hotmail.com>
2022-09-07 18:57:33 -0300

    Upgrade

--- jogoAdivinhação/forca.py
+++ jogoAdivinhação/forca.py
@@ -5,6 +5,23 @@ def jogar():
     print("------Bem vindo ao jogo da forca-----")
     print("--------------------------------------------------------")
 
+    palavra_secreta = "banana"
+  
+    enforcou = False
+    acertou = False
+    
+    while(not enforcou and not acertou):
+        resposta = input("Qual é a palavra secreta?")
+        resposta = resposta.strip()
+        print("jogando...")
+        
+        index = 0
+        
+        for letra in palavra_secreta:
+            if(resposta.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra,index))
+            index = index+1    
+                
 
 
 if (__name__ == '__main__'):
--- jogoAdivinhação/jogos.py
+++ jogoAdivinhação/jogos.py
@@ -1,25 +1,27 @@
 import adivinhacao
 import forca
 
+def escolhe_jogo():
 
-print("--------------------------------------------------------")
-print("*************JOGOS**************")
-print("--------------------------------------------------------")
-print("Selecione o jogo que deseja jogar")
-print("--------------------------------------------------------")
-
-
-
-print("(1) Jogo de adivinhação")
-print("(2) Jogo da forca")
-
-escolha = int(input("Digite o número do seu jogo !"))
+    print("--------------------------------------------------------")
+    print("*************JOGOS**************")
+    print("--------------------------------------------------------")
+    print("Selecione o jogo que deseja jogar")
+    print("--------------------------------------------------------")
 
 
 
-if escolha == 1 :
-    adivinhacao.jogar()
-    
-if escolha == 2 :
+    print("(1) Jogo de adivinhação")
+    print("(2) Jogo da forca")
+
+    escolha = int(input("Digite o número do seu jogo !"))
+    if escolha == 1 :
+        print("Jogando adivinhação")
+        adivinhacao.jogar()
+    if escolha == 2 :
+        print("Jogando forca")
         forca.jogar()
-
+        
+        
+if(__name__ =='__main__'):        
+    escolhe_jogo()
